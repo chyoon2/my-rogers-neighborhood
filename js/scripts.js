19045079963577905ac322027622979833de740d
Chris Yoon <chy.yoon@gmail.com>
2020-06-12 13:33:38 -0700

    Add functionality to .js

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,30 @@
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    var number = $("input#number").val();
+    let number = $("input#number").val();
     let outputArray = [];
-   
-    for (i = 0; i <= number; i++) {
-      if (i.toString().includes(3)) {
-        outputArray.push("Won't you be my neighbor!");
+    neighbor(number);
+
+
+    function neighbor(value) {
+      console.log(value)
+      for (i = 0; i <= value; i++) {
+        if (i.toString().includes(3)) {
+          outputArray.push("Won't you be my neighbor!");
+        }
+        else if (i.toString().includes(2)) {
+          outputArray.push("Boop!");
+        }
+        else if (i.toString().includes(1)) {
+          outputArray.push("Beep!");
+        }
+        else {
+          outputArray.push(i);
+          console.log(outputArray)
+        }
       }
-      else if (i.toString().includes(2)) {
-        outputArray.push("Boop!");
-      }
-      else if (i.toString().includes(1)) {
-        outputArray.push("Beep!");
-      }
-      else {
-        outputArray.push(i);
-      }
-    }
-      $("#output").append(outputArray.join(", "));
-      console.log(outputArray)
+      return $("#output").text(outputArray.join(", "));
+
+    }  
   });
 });
