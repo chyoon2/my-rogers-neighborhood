600ab9ef6b42cd4b4fbb1a3d30e94dea75226ee8
Chris Yoon <chy.yoon@gmail.com>
2020-06-12 15:47:02 -0700

    Add a name input area and concat method to add the name to the end of the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,36 @@
 //Your face logic
 $(document).ready(function() {
-  
   $("button#button2").click(function(event) {
     event.preventDefault();
+    let name = $("input#name").val();
     let reverseNumber = $("input#number").val();
-    $("#output").text(neighbor(reverseNumber).reverse().join(", "));
+    $("#output").text(neighbor(reverseNumber).reverse().join(", ").concat(" Thank you "+ name.toUpperCase()));
+
   });
-  
   $("form#form-one").submit(function(event) {
     event.preventDefault();
+    let name = $("input#name").val();
     let number = $("input#number").val();
-    $("#output").text(neighbor(number).join(", "));
-
+    $("#output").text(neighbor(number).join(", ").concat(" Thank you "+ name.toUpperCase()));
   });  
-
-  
-
 });
-
-
-  function neighbor(value) {
-    let outputArray = [];
-    for (i = 0; i <= value; i++) {
-      if (i.toString().includes(3)) {
-        outputArray.push("Won't you be my neighbor!");
-      }
-      else if (i.toString().includes(2)) {
-        outputArray.push("Boop!");
-      }
-      else if (i.toString().includes(1)) {
-        outputArray.push("Beep!");
-      }
-      else {
-        outputArray.push(i);
-      }
+function neighbor(value) {
+  let outputArray = [];
+  for (i = 0; i <= value; i++) {
+    if (i.toString().includes(3)) {
+      outputArray.push("Won't you be my neighbor!");
     }
-    return outputArray;
-  }  
+    else if (i.toString().includes(2)) {
+      outputArray.push("Boop!");
+    }
+    else if (i.toString().includes(1)) {
+      outputArray.push("Beep!");
+    }
+    else {
+      outputArray.push(i);
+    }
+  }
+  return outputArray;
+}  
 
 
