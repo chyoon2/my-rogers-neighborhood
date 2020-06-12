073a9b67ed837c6f13cd774ffe77ffc3cad54abf
Chris Yoon <chy.yoon@gmail.com>
2020-06-12 09:21:19 -0700

    Add outputs replacing 1,2,3 with quoted outputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,25 @@ $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
     var number = $("input#number").val();
-    outputArray = [];
+    let outputArray = [];
+   
     for (i = 0; i <= number; i++) {
-      outputArray.push(i);
+      if (i === 1) {
+        outputArray.push("Beep!");
+      }
+      else if (i === 2) {
+        outputArray.push("Boop!");
+      }
+      else if (i === 3) {
+        outputArray.push("Won't you be my neighbor!");
+      }
+      else {
+        outputArray.push(i);
+      }
     }
+  
+
+
       $("ul#output").text(outputArray.join(", "));
   });
 });
