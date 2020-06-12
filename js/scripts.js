878bfae62467be35fc1a96acbcdbab87f610a9ef
Chris Yoon <chy.yoon@gmail.com>
2020-06-12 09:31:35 -0700

    Achieve the desired behavior of overiding the larger behavior with the smaller behavior.

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
     let outputArray = [];
    
     for (i = 0; i <= number; i++) {
-      if (i.toString().includes(1)) {
-        outputArray.push("Beep!");
+      if (i.toString().includes(3)) {
+        outputArray.push("Won't you be my neighbor!");
       }
       else if (i.toString().includes(2)) {
         outputArray.push("Boop!");
       }
-      else if (i.toString().includes(3)) {
-        outputArray.push("Won't you be my neighbor!");
+      else if (i.toString().includes(1)) {
+        outputArray.push("Beep!");
       }
       else {
         outputArray.push(i);
