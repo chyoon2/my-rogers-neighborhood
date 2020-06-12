ca89a5b02801bb2206260d9a58aebf8539273c28
Chris Yoon <chy.yoon@gmail.com>
2020-06-12 14:40:48 -0700

    Add a reverse button to html and seperate business logic from user logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,34 @@ $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
     let number = $("input#number").val();
-    let outputArray = [];
-    neighbor(number);
-
-
-    function neighbor(value) {
-      console.log(value)
-      for (i = 0; i <= value; i++) {
-        if (i.toString().includes(3)) {
-          outputArray.push("Won't you be my neighbor!");
-        }
-        else if (i.toString().includes(2)) {
-          outputArray.push("Boop!");
-        }
-        else if (i.toString().includes(1)) {
-          outputArray.push("Beep!");
-        }
-        else {
-          outputArray.push(i);
-          console.log(outputArray)
-        }
-      }
-      return $("#output").text(outputArray.join(", "));
-
-    }  
-  });
+    let mad= neighbor(number);
+    $("#output").text(mad.join(", "));  
+  });  
 });
+
+  $("button#reverse").click(function() {
+  
+    $("#output").append("<li>BABY</li>");
+});
+
+
+  function neighbor(value) {
+    let outputArray = [];
+    for (i = 0; i <= value; i++) {
+      if (i.toString().includes(3)) {
+        outputArray.push("Won't you be my neighbor!");
+      }
+      else if (i.toString().includes(2)) {
+        outputArray.push("Boop!");
+      }
+      else if (i.toString().includes(1)) {
+        outputArray.push("Beep!");
+      }
+      else {
+        outputArray.push(i);
+      }
+    }
+    return outputArray;
+  }  
+
+
